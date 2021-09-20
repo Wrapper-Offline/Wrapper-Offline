d7bd018a6615544e7dc458c3993f7d5102f1e0e1
michelle1574 <70703680+michelle1574@users.noreply.github.com>
2021-09-20 17:34:04 +0200

    Voice Fix toogler
    Includes:
    Downloader
    Acapela voice fix toogler
    Voice fix toogler

---
+++ restore_acapela_voice_fix.cmd
@@ -0,0 +1,35 @@
+@echo off
+cd /d "%~dp0"
+if not exists server goto nothere
+if not exists utilities goto nothere
+if not exists wrapper goto nothere
+if exists wrapper\tts\main.js del wrapper\tts\main.js
+if exists wrapper\tts\info.json del wrapper\tts\info.json
+title Acapela Voice Fix toogler
+choice /c ED /m "Enable voice fix or disable?"
+if errorlevel equ 1 goto enable
+if errorlevel equ 2 goto disable
+
+:enable
+:: Download info.json with the voice fix.
+aria2c -o"wrapper\tts\info.json" "https://raw.githubusercontent.com/michelle1574/Wrapper-Offline-with-Business-Friendly/cbd67d18f2681375159dc9b50eecf4aacd932f7d/wrapper/tts/info.json"
+:: Download main.js with the voice fix.
+aria2c -o"wrapper\tts\main.js" "https://raw.githubusercontent.com/michelle1574/Wrapper-Offline-with-Business-Friendly/cbd67d18f2681375159dc9b50eecf4aacd932f7d/wrapper/tts/main.js"
+goto done
+
+:disable
+:: Download info.json without the voice fix.
+aria2c -o"wrapper\tts\info.json" "https://raw.githubusercontent.com/michelle1574/Wrapper-Offline-with-Business-Friendly/main/wrapper/tts/info.json"
+:: Download main.js without the voice fix.
+aria2c -o"wrapper\tts\main.js" "https://raw.githubusercontent.com/michelle1574/Wrapper-Offline-with-Business-Friendly/main/wrapper/tts/main.js"
+goto done
+
+:done
+echo Done
+pause
+exit
+
+:notthere
+echo This is not Wrapper:offline folder.
+pause
+exit
