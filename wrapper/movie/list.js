e76fb5a1190f61c1047018fc2cee42a0a5cefe34
Joseph The Animator <100003036+josephanimate2021@users.noreply.github.com>
2022-02-20 21:55:43 -0500

    deleted some files to make room for my starters

--- wrapper/movie/list.js
+++
@@ -1,6 +0,0 @@
-const movie = require('./main');
-module.exports = function (req, res, url) {
-	if (req.method != 'GET' || url.path != '/movieList') return;
-	Promise.all(movie.list().map(movie.meta)).then(a => res.end(JSON.stringify(a)));
-	return true;
-}
