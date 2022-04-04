815a927ff8ee009371c6d4677f1135f9758a69a4
Joseph The Animator <100003036+josephanimate2021@users.noreply.github.com>
2022-04-03 21:52:40 -0400

    recaculating

--- wrapper/asset/thmb.js
+++
@@ -1,8 +0,0 @@
-const thumbUrl = process.env.THUMB_BASE_URL;
-const get = require('../request/get');
-
-module.exports = function (req, res, url) {
-	if (req.method != 'GET' || !url.path.startsWith('/stock_thumbs')) return;
-	get(thumbUrl + url.path.substr(url.path.lastIndexOf('/'))).then(v => res.end(v));
-	return true;
-}
--- wrapper/data/heartbeat.js
+++
@@ -1,6 +0,0 @@
-const sessions = require('./sessions');
-module.exports = function (req, res, url) {
-	if (req.method != 'POST' || url.path != '/goapi/heartbeat/v1/') return;
-	res.end(`{"health":"0","locked":"${sessions.getCount(req) > 1 ? 1 : 0}"}`);
-	return true;
-}
