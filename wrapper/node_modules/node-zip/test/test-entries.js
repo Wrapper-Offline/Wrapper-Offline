d1f1c0b7a045a707666101f66874f7db860a3273
Thunderzapper <justinabossmlg@gmail.com>
2022-04-05 09:46:41 -0400

    wrapper: Remove node modules and ignore them

--- wrapper/node_modules/node-zip/test/test-entries.js
+++
@@ -1,9 +0,0 @@
-var zip = require('../');
-
-var zipFile = zip.unzip('test.zip');
-for (var entry of zipFile) {
-    console.log(entry);
-}
-for (var entry of zipFile.entries()) {
-    console.log(entry);
-}
