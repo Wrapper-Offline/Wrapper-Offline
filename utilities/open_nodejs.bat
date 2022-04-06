bfdae1af5363584049830b165e4e24fbcb372494
sparrkz <71980947+sparrkzz@users.noreply.github.com>
2022-04-06 07:41:45 -0400

    this is kinda stupid but i don't have any means of testing something else rn

--- utilities/open_nodejs.bat
+++ utilities/open_nodejs.bat
@@ -6,11 +6,10 @@ pushd "%~dp0"
 title NODE.JS HASN'T STARTED YET
 pushd ..\wrapper
 npm start
+echo Trying to install NPM...
+npm install
+npm start
 echo:
 echo If you see an error saying "npm is not recognized",
 echo please install Node.js from nodejs.org.
-echo:
-echo If you see an error that says "MODULE_NOT_FOUND",
-echo please type "npm install" in this window, press enter,
-echo and then type "npm start" and press enter.
 pause & exit
