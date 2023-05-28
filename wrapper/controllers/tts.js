781fcb52c2c1dbb832393bc6cd4a5bea28f50c43
SimplyA_Coder <107278351+SimplyA-Coder@users.noreply.github.com>
2023-05-28 08:26:38 -0500

    tts was broken, fixed!

--- wrapper/controllers/tts.js
+++ wrapper/controllers/tts.js
@@ -1,15 +1,24 @@
+
+/**
+ * tts routes
+ */
+// modules
 const fs = require("fs");
-const httpz = require("@octanuary/httpz");
+const httpz = require("@octanuary/httpz")
 const tempfile = require("tempfile");
-const Asset = require("../models/asset");
-const { mp3Duration } = require("../../utils/fileUtil");
-const processVoice = require("../models/tts");
+// vars
 const info = require("../data/voices");
+// stuff
+const Asset = require("../models/asset");
+const fileUtil = require("../../utils/realFileUtil");
+const processVoice = require("../models/tts");
+
+// create the group
 const group = new httpz.Group();
 
 /*
-generate the list
-*/
+ * generate the list
+ */
 const voices = info.voices, langs = {};
 Object.keys(voices).forEach((i) => {
 	const v = voices[i], l = v.language;
@@ -19,48 +28,41 @@ Object.keys(voices).forEach((i) => {
 const xml = `${process.env.XML_HEADER}<voices>${
 	Object.keys(langs).sort().map(i => {
 		const v = langs[i], l = info.languages[i];
-		return `<language id="${i}" desc="${l}">${v.join("")}</language>`;
-	}).join("")}</voices>`;
+		return `<language id="${i}" desc="${l}">${v.join('')}</language>`;
+	}).join('')}</voices>`;
 
-/*
-list
-*/
-group.route("POST", "/goapi/getTextToSpeechVoices/", (req, res) => {
-	res.setHeader("Content-Type", "text/html; charset=UTF-8");
-	res.end(xml);
-});
+group
+	// list
+	.route("POST", "/goapi/getTextToSpeechVoices/", (req, res) => {
+		res.setHeader("Content-Type", "text/html; charset=UTF-8");
+		res.end(xml);
+	})
+	// load
+	.route("POST", "/goapi/convertTextToSoundAsset/", async (req, res) => {
+		const { voice, text } = req.body;
+		res.assert(voice, text, 400, "");
 
-/*
-load
-*/
-group.route("POST", "/goapi/convertTextToSoundAsset/", async (req, res) => {
-	const { voice, text } = req.body;
-	res.assert(voice, text, 400, "");
+		try {
+			const filepath = tempfile(".mp3");
+			const writeStream = fs.createWriteStream(filepath);
+			const readStream = await processVoice(voice, text);
+			readStream.pipe(writeStream);
 
-	const filepath = tempfile(".mp3");
-	const writeStream = fs.createWriteStream(filepath);
-	processVoice(voice, text).then((data) => {
-		if (typeof data.on == "function") {
-			data.pipe(writeStream);
-		} else {
-			writeStream.end(data);
-		}
-
-		writeStream.on("close", async () => {
-			const duration = await mp3Duration(filepath);
-			const meta = {
-				duration,
-				type: "sound",
-				subtype: "tts",
-				title: `[${voices[voice].desc}] ${text}`
-			};
-			const id = await Asset.save(filepath, "mp3", meta);
-			res.end(`0<response><asset><id>${id}</id><enc_asset_id>${id}</enc_asset_id><type>sound</type><subtype>tts</subtype><title>${meta.title}</title><published>0</published><tags></tags><duration>${meta.duration}</duration><downloadtype>progressive</downloadtype><file>${id}</file></asset></response>`);
-		});
-	}).catch((err) => {
-		console.error("Error generating TTS:", err);
-		res.end(`1<error><code>ERR_ASSET_404</code><message>${err}</message><text></text></error>`);
+			writeStream.on("close", async () => {
+				const duration = await fileUtil.mp3Duration(filepath);
+				const meta = {
+					duration,
+					type: "sound",
+					subtype: "tts",
+					title: `[${voices[voice].desc}] ${text}`
+				}
+				const id = await Asset.save(filepath, "mp3", meta);
+				res.end(`0<response><asset><id>${id}</id><enc_asset_id>${id}</enc_asset_id><type>sound</type><subtype>tts</subtype><title>${meta.title}</title><published>0</published><tags></tags><duration>${meta.duration}</duration><downloadtype>progressive</downloadtype><file>${id}</file></asset></response>`);
+			});
+		} catch (e) {
+			console.error("Error generating TTS:", e);
+			res.end(`1<error><code>ERR_ASSET_404</code><message>${e}</message><text></text></error>`);
+		};
 	});
-});
 
 module.exports = group;
