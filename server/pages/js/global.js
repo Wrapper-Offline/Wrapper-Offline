f14cd85887c99a15ab225d666983b055f5398d82
octanuary <94416681+octanuary@users.noreply.github.com>
2024-08-07 05:46:12 -0500

    refactor the page system, updates 2 the new design

--- server/pages/js/global.js
+++ server/pages/js/global.js
@@ -1,112 +1,188 @@
-/*
-set dark mode
-*/
-if (localStorage.getItem("DARK_MODE") == "true") {
-	toggleDarkMode();
-}
-
-function toggleDarkMode() {
-	const html = document.documentElement;
-	if (html.classList.contains("dark")) {
-		html.classList.remove("dark");
-		return;
-	}
-	html.classList.add("dark");
-}
-
-/**
- * Called whenever a sidebar link is clicked
- * @param {Event} e 
- */
-function toggleSidebar(e) {
-	const sidebar = document.getElementById("wo_sidebar");
-	sidebar.classList.toggle("collapsed")
-}
-
-/**
- * Called whenever a sidebar link is clicked
- * @param {Event} e 
- * @param {string} loc
- */
-function onPageSwitch(e, loc) {
-	const eExists = e != null && typeof e != "undefined";
-	eExists && e.preventDefault();
-	eExists && e.stopPropagation();
-	eExists && (loc = e.currentTarget.href);
-
-	const main = document.getElementById("wo_page");
-	main.innerHTML = "<marquee>Loading...</marquee>";
-	Page = null;
-
-	const links = document.getElementsByClassName("sidebar_link");
-	for (let i = 0; i < links.length; i++) {
-		const link = links[i];
-		if (link.classList.contains("sel")) {
-			link.classList.remove("sel");
+const Page = {
+	appName: "Wrapper: Offline",
+	sidebarLinks: [
+		{
+			location: "/videos",
+			name: "Videos",
+			icon: "film",
+			default: true
+		},
+		{
+			location: "/starters",
+			name: "Starters",
+			icon: "briefcase"
+		},
+		{
+			location: "/videos",
+			name: "Characters",
+			icon: "person"
 		}
-	}
-	eExists && e.currentTarget.classList.add("sel");
+	],
 
-	eExists && history.pushState({}, "", loc);
-	$.get(loc)
-		.done((data) => {
-			main.innerHTML = data;
-			const scripts = main.getElementsByTagName("script");
-			for (let i = 0; i < scripts.length; i++) {
-				const script = scripts[i];
-				const clone = document.createElement("script");
-				if (script.src) clone.src = script.src;
-				clone.innerHTML = script.innerHTML;
-				script.parentElement.replaceChild(clone, script);
+	init() {
+		if (localStorage.getItem("DARK_MODE") == "true") {
+			Page.toggleDarkMode();
+		}
+		Page.initEvents();
+		Sidebar.instance;
+		Page.runAfter();
+	},
+
+	initEvents: () => {
+		window.addEventListener("popstate", (e) => switchTo(window.location.pathname));
+		$(".tab_navigation .tab").on("click", (event) => {
+			const clicked = $(event.target);
+			const num = clicked.attr("data-triggers");
+			if (num) {
+				// get siblings
+				const buttons = clicked.siblings("a");
+				const pages = clicked.parent().siblings();
+				// toggle button
+				buttons.removeClass("selected");
+				clicked.addClass("selected");
+				// hide other pages and show current one
+				pages.hide()
+				$(pages[num]).show();
 			}
-			Page();
-			initEvents();
-		})
-		.fail((data) => {
-			main.innerText = "An error has occured trying to load this page.";
 		});
-}
+	},
 
-window.addEventListener("popstate", (e) => onPageSwitch(null, window.location.href));
+	toggleDarkMode: () => {
+		const html = document.documentElement;
+		html.classList.toggle("dark");
+	},
 
-function initEvents() {
-	$(".tab_navigation .tab").on("click", (event) => {
-		const clicked = $(event.target);
-		const num = clicked.attr("data-triggers");
-		if (num) {
-			// get siblings
-			const buttons = clicked.siblings("a");
-			const pages = clicked.parent().siblings();
-			// toggle button
-			buttons.removeClass("selected");
-			clicked.addClass("selected");
-			// hide other pages and show current one
-			pages.hide()
-			$(pages[num]).show();
-		}
-	});
-}
+	setTitle: (title) => {
+		document.title = title + " - " + Page.appName;
+	},
 
-/*
-tabs
-*/
-window.addEventListener("load", () => {
-	Page();
-	initEvents();
-	const links = document.getElementsByClassName("sidebar_link");
-	for (let i = 0; i < links.length; i++) {
-		const link = links[i];
-		console.log(link, i, links, links.length)
-		if (link.getAttribute("data-ignore") !== null) {
-			continue;
+	/**
+	 * Called whenever a sidebar link is clicked
+	 * @param {string} loc
+	 */
+	switchTo: (loc) => {
+		const main = document.getElementById("wo_page");
+		main.innerHTML = "<marquee>Loading...</marquee>";
+		this.runAfter = null;
+
+		const links = document.getElementsByClassName("sidebar_link");
+		for (const link of links) {
+			if (link.classList.contains("sel")) {
+				link.classList.remove("sel");
+			}
+			if (link.href == window.location.href) {
+				link.classList.add("sel");
+			}
 		}
-		if (link.getAttribute("data-toggle") !== null) {
-			link.addEventListener("click", toggleSidebar);
-			continue;
-		}
-		link.addEventListener("click", onPageSwitch);
+
+		$.get(loc)
+			.done((data) => {
+				main.innerHTML = data;
+				const scripts = main.getElementsByTagName("script");
+				for (let i = 0; i < scripts.length; i++) {
+					const script = scripts[i];
+					const clone = document.createElement("script");
+					if (script.src)
+						clone.src = script.src;
+					clone.innerHTML = script.innerHTML;
+					script.parentElement.replaceChild(clone, script);
+				}
+				Page.runAfter();
+				Page.initEvents();
+			})
+			.fail((data) => {
+				main.innerText = "An error has occured trying to load this page.";
+			});
+	},
+
+	/**
+	 * Contains page-specific functions and is replaced whenever
+	 * a new page is loaded.
+	 */
+	runAfter: () => null
+};
+
+class Sidebar {
+	constructor() {
+		this.elem = document.getElementById("wo_sidebar");
+		this.sectionPop = document.getElementById("sidebar_populate");	
+		this.addLinks();
+		this.listenEvents();
 	}
-});
+
+	static get instance() {
+		if (typeof this._instance == "undefined")
+			this._instance = new Sidebar();
+		return this._instance;
+	}
+
+	/**
+	 * Adds link elements to the populate section of the sidebar.
+	 */
+	addLinks() {
+		for (const link of Page.sidebarLinks) {
+			const elem = this.createLinkElement(link);
+			this.sectionPop.appendChild(elem);
+		}
+	}
+
+	/**
+	 * Adds event listeners for all link elements that require them.
+	 */
+	listenEvents() {
+		const links = document.getElementsByClassName("sidebar_link");
+		for (const link of links) {
+			if (link.getAttribute("data-ignore") !== null) {
+				continue;
+			}
+			if (link.getAttribute("data-toggle") !== null) {
+				link.addEventListener("click", () => this.onMenuClick());
+				continue;
+			}
+			link.addEventListener("click", this.onLinkClick);
+		}
+	}
+
+	/**
+	 * Creates an element from a link object.
+	 * @param {object} link 
+	 * @returns {HTMLAnchorElement}
+	 */
+	createLinkElement(link) {
+		const elem = document.createElement("a");
+		elem.classList.add("sidebar_link");
+		elem.href = link.location;
+		const icon = document.createElement("i");
+		icon.classList.add("ico", link.icon);
+		elem.appendChild(icon);
+		const text = document.createElement("div");
+		text.classList.add("sb_link_text");
+		text.innerText = link.name;
+		elem.appendChild(text);
+		return elem;
+	}
+
+	/**
+	 * Called whenever a sidebar link is clicked.
+	 * @param {Event} e 
+	 * @param {string} loc
+	 */
+	onLinkClick(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		history.pushState({}, "", e.currentTarget.href);
+		Page.switchTo(e.currentTarget.href);
+	}
+
+	/**
+	 * Called whenever the collapse button is clicked. 
+	 */
+	onMenuClick() {
+		this.elem.classList.toggle("collapsed");
+	}
+}
+
+window.addEventListener("load", Page.init);
 
 /*
 check for updates
